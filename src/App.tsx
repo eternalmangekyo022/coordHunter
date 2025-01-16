5174094effa52b4e785a3a63d0b5c4b3b3a72c94
Tibor Fazekas <tibcso0322@gmail.com>
2025-01-16 23:31:47 +0100

    tesi

--- src/App.tsx
+++ src/App.tsx
@@ -104,6 +104,9 @@ export default function App() {
     document.querySelector('body')?.appendChild(a)
     a.click()
     document.querySelector('body')?.removeChild(a)
+    //pokemongo://spprotele=51.93934,7.71475
+
+    //pokemongo://spprotele=34.062701,-118.290082
 
     setCoords(shortest);
   }
@@ -131,6 +134,6 @@ export default function App() {
       <option value="taipei">Taipei</option>
     </select>
     <span className={`ready${ready ? ' active' : ''}`}>{ready ? 'Coords are ready, now: ' : 'No coords found, wrong input'}{coords.length ? <span className='coord'>#{current + 1} {coords[current].coord}{current === coords.length - 1 ? '': ' Next ' + (coords[current].distanceNext || 0).toFixed(2) + 'km'}</span>: null}</span>
-    <a className='next-btn' target='_blank' href={coords.length > 0 ? `pokemongo://spprotele=${coords[current].coord}`: ''} onClick={next}>Next</a>
+    <a className='next-btn' target='_blank' object='link-editable' href={coords.length > 0 ? `pokemongo://spprotele=${coords[current].coord}`: ''} onClick={next}>Next</a>
   </div>
 }
