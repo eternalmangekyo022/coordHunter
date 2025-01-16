b3231ed810260807a32989d87c6d5f8bfa1fbb7d
Tibor Fazekas <tibcso0322@gmail.com>
2025-01-17 00:12:41 +0100

    tesi

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
 export default function Button({ onClick, href }) {
-	return <a href="pokemongo://spprotele=54.09744,13.40384" target="_blank" style="text-decoration:none; color:#ffffff; line-height:31px; display:block;" object="link-editable">SpooferPro</a>
+	return <a href="pokemongo://spprotele=54.09744,13.40384" target="_blank" style={{ textDecoration: 'none', color: '#ffffff', lineHeight: '31px', display: 'block' }} object="link-editable">SpooferPro</a>
 
 }
