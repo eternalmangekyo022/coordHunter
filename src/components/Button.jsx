0af1e771057daa325d53e58a547c7c10632573de
Tibor Fazekas <tibcso0322@gmail.com>
2025-01-17 00:06:27 +0100

    bla

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,4 @@
 export default function Button({ onClick, href }) {
 	return <a href="pokemongo://spprotele=54.09744,13.40384" target="_blank" style="text-decoration:none; color:#ffffff; line-height:31px; display:block;" object="link-editable">SpooferPro</a>
 
-	return (
-		<a className='next-btn' target='_blank' object='link-editable' href={href} onClick={onClick}>Next</a>
-	)
 }
